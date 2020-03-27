45835858228d64bf9ee0ecdae75da105dff4ef84
Bahadir Hocamoglu <bahadirhocamoglu@gmail.com>
2020-03-27 17:29:31 +0300

    update cors settings

--- index.js
+++ index.js
@@ -9,8 +9,15 @@ const { sessionSecret, PORT } = require('./config')
 const app = require('express')()
 const routes = require('./routes')
 
-app.use(helmet()) // TODO: fine-tuning?
-app.use(cors({ credentials: true, origin: /.*/ }))
+// app.use(helmet({permittedCrossDomainPolicies: true, })) // TODO: fine-tuning?
+app.use(
+  cors({
+    credentials: true,
+    origin: true,
+    preflightContinue: true,
+    optionsSuccessStatus: 200,
+  })
+)
 app.use(
   session({
     name: 'testingApp',
