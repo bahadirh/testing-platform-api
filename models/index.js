4b2a5d9abcb75e8ddb2ada408fd362f165fbb326
Bahadir Hocamoglu <bahadirhocamoglu@gmail.com>
2020-04-17 13:57:38 +0300

    Add error message to db connection failures

--- models/index.js
+++ models/index.js
@@ -7,14 +7,19 @@ const file = require('./files')
 const suite = require('./suites')
 const case_ = require('./cases')
 
-mongoose.connect(dbURL, {
-  auth: { user: dbUsername, password: dbPassword },
-  autoIndex: false,
-  useCreateIndex: true,
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-})
+mongoose
+  .connect(dbURL, {
+    auth: { user: dbUsername, password: dbPassword },
+    autoIndex: false,
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .catch((error) => {
+    console.error('Unable to connect.')
+    console.error(error.message)
+  })
 
 module.exports = {
   ...user,
