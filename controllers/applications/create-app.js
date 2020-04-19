368ee022d7fa4df6dfc26e123b9b6ce5e1be9cbf
Bahadir Hocamoglu <bahadirhocamoglu@gmail.com>
2020-04-19 12:58:49 +0300

    Return status 400 on app creation error

--- controllers/applications/create-app.js
+++ controllers/applications/create-app.js
@@ -16,7 +16,7 @@ const createAppHandler = (req, res, next) => {
 
 const createAppErrorHandler = (error, req, res, next) => {
   // TODO: handle errors
-  res.json({ status: 'error', message: error.message })
+  res.status(400).json({ status: 'error', message: error.message })
 }
 
 module.exports = [createAppHandler, createAppErrorHandler]
